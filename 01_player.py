1ed4c29cc070a30a1f5086446d3391d6e6a0f5c0
Nelson Mu <nmu@e1z2r8p2.42.us.org>
2018-06-19 15:14:55 -0700

    oops

--- 01_player.py
+++
@@ -1,3 +0,0 @@
-print("Hello hacker, what is your name?")
-name = input(">")
-print("Welcome, " + name + "!")
--- 02_daenerys.py
+++
@@ -1,8 +0,0 @@
-print("Who goes there?")
-name = (input(">"))
-if name == "Daenerys of the House Targaryen, the First of Her Name, The Unburnt, Queen of the Andals, the Rhoynar and the First Men, Queen of Meereen, Khaleesi of the Great Grass Sea, Protector of the Realm, Lady Regnant of the Seven Kingdoms, Breaker of Chains and Mother of Dragons":
-	print("welcome, Daenerys.")
-elif name == "Dany":
-	print("Dany who?")
-else:
-	print("Move along, now.")
