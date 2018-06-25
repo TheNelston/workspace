860920d7b41523b37ae5e6d05557cfdd1e46ac29
Nelson Mu <nmu@e1z2r8p2.42.us.org>
2018-06-25 15:03:14 -0700

    deez nuts

---
+++ 00_rainbow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#By nmu
+#This program displays your text in rainbow colors! 
+import sys
+def print__colorful__text(string, style, foreground, background):
+	for style in range(8):
