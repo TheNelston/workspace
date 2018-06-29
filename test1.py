72c0644e6da428359225a7d0844f9c5d533d40b0
Nelson Mu <nmu@e1z2r8p2.42.us.org>
2018-06-29 13:52:06 -0700

    day03's work

--- test1.py
+++
@@ -1,8 +0,0 @@
-for name in fnoccurcount:
-	l = []
-	i = 0
-	while i < fnoccurcount[name]:
-		l.append(classmates[classmates.index(name)+1])
-		classmates.remove(classmates[classmates.index(name)
-		i += 1
-	commfnames.update(dict.fromkeys(l, name))
